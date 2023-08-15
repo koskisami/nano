58c94bb1f79639e28f87461fd05113cd89c471c8
Sami Koski <132483274+koskisami@users.noreply.github.com>
2023-08-15 23:42:03 +0300

    added presence for bot
    also its nano now

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,16 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		client.user.presence.set({
+			status: "dnd",
+			activities: [{
+				type: ActivityType.Competing,
+				name: "water melon eatjnf"
+			}]
+		});
 	},
 };
